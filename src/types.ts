6c1da5030443eb30147d1493aee8c6a91c5f3c45
RamosJSouza <ramosinfo@gmail.com>
2025-10-21 15:24:19 -0300

    types definitnions

---
+++ src/types.ts
@@ -0,0 +1,59 @@
+export interface People {
+    name: string;
+    birth_year: string;
+    eye_color: string;
+    gender: string;
+    hair_color: string;
+    height: string;
+    mass: string;
+    skin_color: string
+    homeworld: string;
+    films: string[];
+    species: string[];
+    starships: string[];
+    vehicles: string[];
+    url: string;
+    created: string;
+    edited: string;
+}
+
+export interface Planets {
+    name: string;
+    diameter: string;
+    rotation_period: string;
+    orbital_period: string;
+    gravity: string;
+    population: string;
+    climate: string;
+    terrain: string;
+    surface_water: string;
+    residents: string[];
+    films: string[];
+    url: string;
+    created: string;
+    edited: string;
+}
+
+export interface Films {
+    title: string;
+    episode_id: number;
+    opening_crawl: string;
+    director: string;
+    producer: string;
+    release_date: string;
+    species: string[];
+    starships: string[];
+    vehicles: string[];
+    characters: string[];
+    planets: string[];
+    url: string;
+    created: string;
+    edited: string;
+}
+
+export interface SearchResponse<T> {
+    count: number,
+    next: string | null;
+    previous: string | null;
+    results: T[];
+}
